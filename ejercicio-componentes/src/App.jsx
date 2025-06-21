76f585e507e8d39cbc6dcce47772040120ffbf87
Paula <paulasanjosemartin@gmail.com>
2025-06-21 12:04:43 +0200

    ejercicios rutas

--- ejercicio-componentes/src/App.jsx
+++ ejercicio-componentes/src/App.jsx
@@ -1,20 +1,38 @@
 
-
+import {Routes, Route, Link, NavLink} from 'react-router'
+import Home from './components/Home'
+import Overview from './components/Overview'
+import Repositories from './components/Repositories'
 
 function App() {
  
 
   return (
-    <>
-  <Routes>
-      <Route >
-        
-      </Route>
-    </Routes>
+<>
+    <nav>  
+    <ul>
+      <li>
+      <NavLink to="/"> Home</NavLink>
+        {" | "}
+        <NavLink to="/overview"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }>
+          Overview
+        </NavLink>
+        {" | "}
+        <NavLink to="/repositories"> Repositories </NavLink>
+      </li>
+    </ul>
+  </nav>
  
-
-  
-    </>
+  <Routes>
+    <Route index path="/" element={<Home/>}/>
+    <Route path="/overview" element={<Overview/>}/>
+    <Route path="/repositories" element={<Repositories/>}/>
+    <Route path="*" element={<h2>Error 404. Página no encontrada.</h2>}/>
+  </Routes>
+</>
 ) 
 }
 
---
+++ ejercicio-componentes/src/components/Repositories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Repositories() {
+  return (
+<p><b>Repositories: </b>Lorem ipsum dolor sit amet consectetur adipiscing elit in, dictum tortor integer mollis augue nisi primis, congue quis laoreet ridiculus etiam sociosqu feugiat. Senectus elementum non sollicitudin ante pulvinar pellentesque, fermentum gravida aliquam orci nam hendrerit vestibulum, lacinia nunc inceptos ultrices aenean. Ornare ridiculus lobortis cum congue tortor felis pulvinar malesuada sem ullamcorper laoreet, duis facilisi mattis luctus scelerisque vitae per primis penatibus habitasse, lacinia cubilia molestie accumsan gravida mauris elementum quisque praesent potenti.</p>
+  )
+}
+
+export default Repositories
